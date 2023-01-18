a090887829483db9c9bdfbb80b051b4b9db4692f
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-18 10:56:03 +0000

    関数リテラル

--- eval2.js
+++
@@ -1,3 +0,0 @@
-var obj = { hoge: 1, foo: 2 };
-var prop = 'hoge';
-eval('console.log(obj.'+ prop + ')');
--- function_obj.js
+++
@@ -1,2 +0,0 @@
-var getTriangle = new Function('base', 'height', 'return base*height/2;');
-console.log('三角形の面積：' + getTriangle(5,2));
