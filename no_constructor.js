4d36aebd59d7c0c58ada027f784658e4a9d59e13
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-20 06:40:33 +0000

    コンストラクターの強制的な呼び出し

---
+++ no_constructor.js
@@ -0,0 +1,9 @@
+var Member = function(firstMame, lastName) {
+   this.firstName = firstMame;
+   this.lastName = lastName;
+};
+
+var m = Member('権兵衛', '佐藤');
+console.log(m);
+console.log(firstName);
+console.log(m.firstName);
