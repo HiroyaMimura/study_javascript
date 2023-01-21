93bb0b1e3e19a3e223b4543850c82fbd7c875838
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-21 06:44:52 +0000

    アプリを機能単位にまとめる/モジュール

---
+++ main2.js
@@ -0,0 +1,4 @@
+import * as app from './lib/Util.js'
+
+var m = new app.Member('太郎', '山田');
+console.log(m.getName());
---
+++ main_default.js
@@ -0,0 +1,2 @@
+import Area from './lib/Area.js';
+console.log(Area.getTriangle(10,5));
