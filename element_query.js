443700a4ea4c4767c8d10135dbd309b4458db53a
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-22 12:28:26 +0000

    querySelector/querySelectorAllメソッド

--- element_query.js
+++ element_query.js
@@ -1,5 +1,4 @@
-var list = document.getElementsByClassName('my');
-
+var list = document.querySelectorAll('#list .external');
 for(var i=0, len=list.length; i<len; i++) {
     console.log(list.item(i).href);
 }
