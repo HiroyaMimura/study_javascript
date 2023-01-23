4dd3f9c1a565d9666a34555631e9d3fc3d03be6c
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-23 07:04:07 +0000

    ラジオボタンの設定

--- radio.js
+++ radio.js
@@ -1,19 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var getRadioValue = function(name) {
-        var result = '';
+    var setRadioValue = function(name, value) {
         var elems = document.getElementsByName(name);
         
         for(var i=0, len=elems.length; i<len; i++) {
             var elem = elems.item(i);
-            if(elem.checked) {
-                result = elem.value;
+            if(elem.value === value) {
+                elem.checked = true;
                 break;
             }
         }
-        return result;
     };
     
-    document.getElementById('btn').addEventListener('click', function() {
-        window.alert(getRadioValue('food'));
-    }, false);
+    setRadioValue('food', '餃子');
     }, false);
