03e3229a5ec583e0bd287724791aa79aba708a44
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-23 06:37:39 +0000

    入力ボックス/選択ボックスの値を取得

--- text_select.js
+++ text_select.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('btn').addEventListener('click', function() {
         var name = document.getElementById('name');
-        var result = document.getElementById('result');
-        result.innerHTML = 'こんにちは、' + name.value + 'さん！';
+        console.log(name.value);
     }, false);
     }, false);
