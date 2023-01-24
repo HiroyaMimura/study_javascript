7315775299b8be923f75fd2dbd069214dc10f346
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-24 05:35:24 +0000

    外部のスタイルシートを適用

--- style_class.js
+++ style_class.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
    var elem = document.getElementById('elem');
    
    elem.addEventListener('mouseover', function() {
-     this.style.backgroundColor = 'yellow';
+     this.className = 'highlight';
    }, false);
    
    elem.addEventListener('mouseout', function() {
-     this.style.backgroundColor = '';
+     this.className = '';
    }, false);
   }, false);
