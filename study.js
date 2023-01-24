2bd0f81ebb9e14a0d5c2442ba09cceb6a79fa31d
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-24 08:32:04 +0000

    イベントの伝播をキャンセル

--- study.js
+++ study.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
    document.getElementById('inner').addEventListener('click', function(e) {
      window.alert('#innerリスナーが発生しました');
-   }, true);
+     e.stopImmediatePropagation();
+   }, false);
    
    document.getElementById('inner').addEventListener('click', function(e) {
      window.alert('#innerリスナー2が発生しました');
-   }, true);
+   }, false);
    
    document.getElementById('outer').addEventListener('click', function(e) {
      window.alert('#outerリスナーが発生しました');
-   }, true);
+   }, false);
 }, false);
