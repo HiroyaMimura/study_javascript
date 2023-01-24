987cc958f7ffdd2a327dc0878f2d2a3d4c510e46
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-24 08:50:51 +0000

    handleEventメソッド

--- study.js
+++ study.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
    var data = {
      title: 'Javaポケットリファレンス',
      price: 2680,
-     show: function() {
+     handleEvent: function() {
        console.log(this.title + '/' + this.price + '円');
      }
    };
    
    document.getElementById('btn').addEventListener(
-     'click', data.show.bind(data), false);
+     'click', data, false);
 }, false);
