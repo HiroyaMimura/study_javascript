da8f8bdc4de7ca4b46e2f1b3ffbbac0c5c13ca51
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-25 05:29:48 +0000

    アプリ単位にデータを保存

--- study.js
+++ study.js
@@ -1,6 +1,19 @@
-var storage = localStorage;
-for(var i=0, len=storage.length; i<len; i++) {
-   var k = storage.key(i);
-   var v = storage[k];
-   console.log(k + ':' + v);
-}
+var MyStorage = function(app) {
+   this.app = app;
+   this.storage = lacalStorage;
+   this.data = JSON.parse(this.storage[this.app] || '{}');
+};
+
+MyStorage.prototype = {
+   getItem: function(key) {
+      return this.data[key];
+   },
+   
+   setItem: function(key, value) {
+      this.data[key] = value;
+   },
+   
+   save: function() {
+      this.storage[this.app] = JSON.stringify(this.data);
+   }
+};
