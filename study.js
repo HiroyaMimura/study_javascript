92b7e586f6c4dcd7ab77dd8a4593f92d34bce966
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-25 03:01:02 +0000

    setIntervalメソッド

--- study.js
+++ study.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-   document.getElementById('fm').addEventListener('submit', function(e) {
-      if(!window.confirm('ページを送信してもいいですか？')) {
-         e.preventDefault();
-      }
-   }, false);
+   var timer = window.setInterval(
+      function() {
+         var dat = new Date();
+         document.getElementById('result').textContent = dat.toLocaleTimeString();
+      }, 1000);
+      
+      document.getElementById('btn').addEventListener('click', function() {
+         window.clearInterval(timer);
+      }, false);
 }, false);
