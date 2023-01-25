586d4b6fd7563c0dd5023f9e91ef088949d3c6ad
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-25 03:24:27 +0000

    locationオブジェクト

--- study.js
+++ study.js
@@ -1,21 +1,5 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//    var timer = window.setInterval(
-//       function() {
-//          var dat = new Date();
-//          document.getElementById('result').textContent = dat.toLocaleTimeString();
-//       }, 1000);
-      
-//       document.getElementById('btn').addEventListener('click', function() {
-//          window.clearInterval(timer);
-//       }, false);
-// }, false);
-
-function hoge () {
-   console.log('あいうえお');
-   setTimeout(function() {
-      console.log('かきくけこ');
-   }, 0);
-   console.log('さしすせそ');
-}
-
-hoge();
+document.addEventListener('DOMContentLoaded', function() {
+   document.getElementById('isbn').addEventListener('change', function() {
+      location.replace('http://www.wings.msn.to/index.php/-/A-03/' + this.value);
+   }, false);
+}, false);
