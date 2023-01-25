99a4c45becf974b9cbffec25f8d5679d78881323
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-25 05:15:33 +0000

    ストレージから全てのデータを取り出す

--- study.js
+++ study.js
@@ -1,7 +1,6 @@
 var storage = localStorage;
-storage.setItem('fruit1', 'りんご');
-storage.fruit2 = 'みかん';
-storage['fruit3'] = 'ぶどう';
-console.log(storage.getItem('fruit1'));
-console.log(storage.fruit2);
-console.log(storage['fruit3']);
+for(var i=0, len=storage.length; i<len; i++) {
+   var k = storage.key(i);
+   var v = storage[k];
+   console.log(k + ':' + v);
+}
