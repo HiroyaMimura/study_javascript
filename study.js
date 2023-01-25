b2da9d094926ab7902999d8dcfe7425003f01967
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-25 03:08:16 +0000

    setTimeoutメソッド

--- study.js
+++ study.js
@@ -1,11 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-   var timer = window.setInterval(
-      function() {
-         var dat = new Date();
-         document.getElementById('result').textContent = dat.toLocaleTimeString();
-      }, 1000);
+// document.addEventListener('DOMContentLoaded', function() {
+//    var timer = window.setInterval(
+//       function() {
+//          var dat = new Date();
+//          document.getElementById('result').textContent = dat.toLocaleTimeString();
+//       }, 1000);
       
-      document.getElementById('btn').addEventListener('click', function() {
-         window.clearInterval(timer);
-      }, false);
-}, false);
+//       document.getElementById('btn').addEventListener('click', function() {
+//          window.clearInterval(timer);
+//       }, false);
+// }, false);
+
+function hoge () {
+   console.log('あいうえお');
+   setTimeout(function() {
+      console.log('かきくけこ');
+   }, 0);
+   console.log('さしすせそ');
+}
+
+hoge();
