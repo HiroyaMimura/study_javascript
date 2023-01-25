a10a3d972691f8079ae6bf277c9eb4def721fec8
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-25 08:36:31 +0000

    Ajaxの利点

--- study.js
+++ study.js
@@ -1,6 +1,21 @@
-window.addEventListener('storage', function(e) {
-   console.log('変更されたキー：' + e.key);
-   console.log('変更前の値：' + e.oldValue);
-   console.log('変更後の値：' + e.newValue);
-   console.log('発生元のページ：' + e.url);
+document.addEventListener('DOMContentLoaded', function() {
+   document.getElementById('btn').addEventListener('click', function() {
+      var result = document.getElementById('result');
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+         if(xhr.readyState === 4) {
+            if(xhr.status === 200) {
+               result.textContent = xhr.responseText;
+            } else {
+               result.textContent = 'サーバーエラーが発生しました';
+            }
+         } else {
+            result.textContent = '通信中...';
+         }
+      };
+      
+      xhr.open('GET', 'study.php?name=' + 
+         encodeURIComponent(document.getElementById('name').value), true);
+         xhr.send(null);
+      }, false);
 }, false);
