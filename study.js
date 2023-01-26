daf43225c94ea1a5a032e2a48897111fc892a8ea
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-26 07:34:53 +0000

    JSDoc

--- study.js
+++ study.js
@@ -1,18 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-   document.getElementById('btn').addEventListener('click', function() {
-      var worker = new Worker('worker.js');
-      
-      worker.postMessage({
-         'target': document.getElementById('target').value,
-         'x': document.getElementById('x').value
-      });
-      document.getElementById('result').textContent = '計算中...';
-      
-      worker.addEventListener('message', function(e) {
-         document.getElementById('result').textContent = e.data;
-      }, false);
-      worker.addEventListener('error', function(e) {
-         document.getElementById('result').textContent = e.message;
-      }, false);
-   }, false);
-}, false);
+/**
+ * @constructor
+ * @classdesc メンバーのついての情報を管理します
+ * @param {string} firstName 性
+ * @param {string} lastName 名
+ * @throws {Error} firstNameかlastNameが足りません
+ * @author Yoshihiro Yamada
+ * @version 1.0.0
+ */
+ var Member = function(firstName, lastName) {
+    if(firstName === undefined || lastName === undefined) {
+       throw new Error('firstNameかlastNameが足りません');
+    }
+    this.firstName = firstName;
+    this.lastName = lastName;
+ };
+ 
+/**
+ * メンバーに関する詳細情報を表示します
+ * @return {String} メンバーの氏名
+ * @deprecated {@lend Member#toString}メソッドを代わりに利用してください
+ */
+ Member.prototype.getName = function() {
+    return this.lastName + '' + this.firstName;
+ };
+ 
+ /**
+  * Memberクラスの内容を文字列化します
+  * @return {String} メンバーの氏名
+  */
+  Member.prototype.toString = function() {
+     return this.lastName + '' + this.firstName;
+  };
