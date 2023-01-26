7adf3b767c128fc020f7672db3b7fdcaa45ddb34
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-26 04:04:29 +0000

    クロスドキュメントメッセージングによるクロスオリジン通信/メッセージ送信

--- study.js
+++ study.js
@@ -1,26 +1,8 @@
-document.getElementById('btn').addEventListener('click', function() {
-   var url = 'https://b.hatena.ne.jp/entry/jsonlite/?callback=show&url=' + encodeURIComponent(document.getElementById('url').value);
-   var scr = document.createElement('script');
-   scr.src = url;
-   document.getElementsByTagName('body').item(0).appendChild(scr);
+document.addEventListener('DOMContentLoaded', function() {
+   var target = 'study.html';
+   document.getElementById('btn').addEventListener('click', function() {
+      document.getElementById('frame').contentWindow.postMessage(
+         document.getElementById('message').value, target);
    }, false);
    
-   function show(data) {
-      if(data === null) {
-         result.textContent = 'ブックマークは存在しませんでした';
-      } else {
-         var bms = data.bookmarks;
-         var ul = document.createElement('ul');
-         for(var i=0; i<bms.length; i++) {
-            var li = document.createElement('li');
-            var anchor = document.createElement('a');
-            anchor.href = 'http://b.hatena.ne.jp/' + bms[i].user;
-            var text = document.createTextNode(
-               bms[i].user + '' + bms[i].comment);
-               anchor.appendChild(text);
-               li.appendChild(anchor);
-               ul.appendChild(li);
-               result.appendChild(ul);
-         }
-      }
-   } 
+}, false);
